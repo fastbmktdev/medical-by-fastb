6565a256a0dc19e9bb327251f7bdb2edac401715
sryongkh <sry.ongkh@gmail.com>
2025-11-04 00:57:07 +0700

    Enhance booking and notification systems

--- src/lib/email/index.ts
+++ src/lib/email/index.ts
@@ -4,5 +4,26 @@ export {
   sendWelcomeEmail,
   isEmailServiceConfigured,
   getEmailServiceStatus,
-  type ContactEmailData
+  type ContactEmailData,
+  // Booking Emails
+  sendBookingConfirmationEmail,
+  sendBookingReminderEmail,
+  type BookingConfirmationData,
+  type BookingReminderData,
+  // Payment Emails
+  sendPaymentReceiptEmail,
+  sendPaymentFailedEmail,
+  type PaymentReceiptData,
+  type PaymentFailedData,
+  // Partner Emails
+  sendPartnerApprovalEmail,
+  sendPartnerRejectionEmail,
+  type PartnerApprovalData,
+  type PartnerRejectionData,
+  // Admin Alerts
+  sendAdminAlertEmail,
+  type AdminAlertData,
 } from './resend';
+
+// Email Templates (for reference/testing)
+export * from './templates';
